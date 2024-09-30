0215817e6912569fe2166dc1d811a61ae8ec0ee3
yogesh <yogesh.mobyink@gmail.com>
2024-09-30 18:31:02 +0530

    department and doctors model

--- src/models/department.model.js
+++ src/models/department.model.js
@@ -12,34 +12,14 @@ const doctorSchema = new Schema(
       required: true,
       trim: true,
     },
-    department: {
-      type: Schema.Types.ObjectId,
-      ref: "Department",
-      required: true,
-    },
-    designation: {
+    bannerImage: {
       type: String,
       required: true,
       trim: true,
     },
-    availablity: {
-      type: [String],
-      required: true,
-    },
-    floor: {
+    content: {
       type: String,
       required: false,
-      trim: true,
-    },
-    room: {
-      type: String,
-      required: false,
-      trim: true,
-    },
-    about: {
-      type: String,
-      required: true,
-      trim: true,
     },
     status: {
       type: Boolean,
