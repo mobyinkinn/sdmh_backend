237c5badd94ef5f840ae49f36ea3088dbb11b82d
yogesh <yogesh.mobyink@gmail.com>
2024-10-02 15:55:43 +0530

    admin controller

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -2,6 +2,25 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Admin } from "../models/admin.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt, { decode } from "jsonwebtoken";
+
+const generateAccessAndRefreshTokens = async (adminId) => {
+  try {
+    const admin = await Admin.findById(adminId);
+    const accessToken = admin.generateAccessToken();
+    const refreshToken = admin.generateRefreshToken();
+
+    admin.refreshToken = refreshToken;
+    await admin.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "something went wrong while creating refresh and access tokens"
+    );
+  }
+};
 
 const registerAdmin = asyncHandler(async (req, res) => {
   /**
@@ -91,4 +110,130 @@ const registerAdmin = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdAdmin, "Admin created successfully"));
 });
 
-export { registerAdmin };
+const loginAdmin = asyncHandler(async (req, res) => {
+  /**
+   * 1. body -> data
+   * 2. username or email
+   * 3. find the user
+   * 4. password check
+   * 5. access and refresh token generation
+   * 6. send cookies (token)
+   * 7. send response
+   */
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new ApiError(400, "email and password is required");
+  }
+
+  const admin = await Admin.findOne({ email });
+
+  if (!admin) {
+    throw new ApiError(404, "Admin does not exist");
+  }
+
+  const isPasswordValid = await admin.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid credentails");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    admin._id
+  );
+
+  const loggedInAdmin = await Admin.findById(admin._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { admin: loggedInAdmin, accessToken, refreshToken },
+        "Admin logged in successfully"
+      )
+    );
+});
+
+const logoutAdmin = asyncHandler(async (req, res) => {
+  Admin.findByIdAndUpdate(
+    req.admin._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"));
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  try {
+    const incomingRefreshToken = req.cookies.refreshToken;
+
+    if (!incomingRefreshToken) {
+      throw new ApiError(401, "unauthorized request");
+    }
+
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const admin = await Admin.findById(decodedToken?._id);
+
+    if (!admin) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+
+    if (incomingRefreshToken !== admin?.refreshToken) {
+      throw new ApiError(401, "Refresh token expired");
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefreshTokens(admin._id);
+
+    return res
+      .status(200)
+      .cookie("acessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Acess token refreshed"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token");
+  }
+});
+
+export { registerAdmin, loginAdmin, logoutAdmin, refreshAccessToken };
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { registerAdmin } from "../controllers/admin.controller.js";
+import {
+  loginAdmin,
+  logoutAdmin,
+  refreshAccessToken,
+  registerAdmin,
+} from "../controllers/admin.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(registerAdmin);
+router.route("/login").post(loginAdmin);
+
+//secured routes
+router.route("/logout").post(verifyJwt, logoutAdmin);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
