dc893061e999b3a644e185f24267000dc67f5f5e
yogesh <yogesh.mobyink@gmail.com>
2025-01-11 18:30:11 +0530

    dfadf

--- src/app.js
+++ src/app.js
@@ -47,7 +47,6 @@ app.use("/api/v1/events", eventRouter);
 app.use("/api/v1/blogs", blogsRouter);
 app.use("/api/v1/downloadables", downloadableRouter);
 app.use("/api/v1/notices", noticeRouter);
-app.use("/api/v1/pages", pagesRouter);
 app.use("/api/v1/awards", awardsRouter);
 app.use("/api/v1/enquiry", enquiryRouter);
 app.use("/api/v1/contact", contactRouter);
